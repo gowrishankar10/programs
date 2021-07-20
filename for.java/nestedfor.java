1c5f186be6cc0781c8603db178f51cfe8a6b7c32
gowrish1998 <gowr982@gmail.com>
2021-07-20 12:46:15 +0530

    java

---
+++ for.java/nestedfor.java
@@ -0,0 +1,19 @@
+package whileloop;
+
+public class forloopings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int count = 1; count<=5; count++) {
+			for (int no = 1; no <=5; no++)
+				
+				System.out.print(no+" ");
+			{
+				System.out.println();
+			}
+			System.out.println();
+
+		}
+	}}
+
