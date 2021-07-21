b9361778727803cd04255ae3b511ada51118dc0d
gowrish1998 <gowr982@gmail.com>
2021-07-21 20:05:01 +0530

    java

---
+++ for.java/star.java
@@ -0,0 +1,23 @@
+package whileloop;
+
+public class starfor {
+
+	public static void main(String[] args) {
+		int  i = 1;
+		
+		for(int row =5;row>1;row--)
+		{
+			for(int colm=1;colm<=row;colm++)
+			{
+			System.out.print("*");
+			}
+			
+	
+		System.out.println();
+		i++;
+		}
+	}
+
+}
+
+
